301c6c55b80033c93b8676e8a0ccaa1188f4aa66
OlwethuB <olwethu.a.bhiya@gmail.com>
2023-08-14 12:17:19 +0200

    mysql_connection

---
+++ api/config/index.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql");
+
+// Create connection to db
+
+const con = mysql.createConnection ({
+    host: "b1d0nyf94l0v4kd07eeb-mysql.services.clever-cloud.com",
+    user: "uiklb15btpljl2qq",
+    password: "Qcv4yOvU5FEbSL1uY6b9"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!")
+});
